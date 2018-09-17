59b1f4059d53a9adff0caa61f95deab83b0dd34c
eduardotp1 <edu.tirta@gmail.com>
2018-09-16 21:21:39 -0300

    values dono_animal

--- backend/routes/pets.js
+++ backend/routes/pets.js
@@ -16,7 +16,7 @@ router.post('/animal', function(req, res, next) {
   var post = req.body;
   try{
     connection.query('START TRANSACTION')
-    connection.query('INSERT INTO animal (Nome,idRaca,idTipo,Sexo,dataNascimento) VALUES (?,?,?,?,?); INSERT INTO dono_animal VALUES (SELECT max(idAnimal) FROM animal where Nome="?"),(SELECT idDono FROM dono where RG="?")',post.Nome,post.Raca,post.Tipo,post.Sexo,post.dataNascimento,post.Nome,post.RG)
+    connection.query('INSERT INTO animal (Nome,idRaca,idTipo,Sexo,dataNascimento) VALUES (?,?,?,?,?); INSERT INTO dono_animal (idAnimal,idDono) VALUES (SELECT max(idAnimal) FROM animal where Nome="?"),(SELECT idDono FROM dono where RG="?")',post.Nome,post.Raca,post.Tipo,post.Sexo,post.dataNascimento,post.Nome,post.RG)
     connection.query('COMMIT')
   }
   catch(err){
